6317d22219c295457b854b42bd784bb3ea423513
Ronik Halai <ronikhalai56@gmail.com>
2024-11-04 11:59:11 +0530

    Modified user model and role model to use soft deletes

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Spatie\Permission\Models\Role as SpatieRole;
 use App\Models\Permission;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends SpatieRole
 {
+    use SoftDeletes;
+
     public function permissions() : BelongsToMany
     {
         return $this->belongsToMany(Permission::class, 'role_has_permissions', 'role_id', 'permission_id');
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Log;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
